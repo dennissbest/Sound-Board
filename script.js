1fa1adb620d4e9f78a56f9adfe700a22afff74b4
DennyyBoyy <bestds@vcu.edu>
2026-09-09 11:21:36 -0400

    Updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Create an array of sounds
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong', 'laugh', 'boing'];
 
 // For each sound, create a button, add a class 'btn", update the text, appdend to DOM
 sounds.forEach((sound) => {
@@ -12,6 +12,21 @@ sounds.forEach((sound) => {
     // Set the button label text to be the same as the string value in the sounds array
     btn.innerText = sound;
 
+    // Add an event listener to this button
+    btn.addEventListener('click', () => {
+        // I ended up removing the stop sounds button as I liked the idea of the choas of them "talking" over eachtoehr 
+        document.getElementById(sound).play();
+    });
+
     // Add the button to the DOM
     document.getElementById('buttons').appendChild(btn);
 });
+
+function stopSounds() {
+    sounds.forEach((sound) => {
+        const snd = document.getElementById(sound);
+
+        snd.pause();
+        snd.currentTime = 0;
+    });
+}
